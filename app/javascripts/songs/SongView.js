bb57112315ecb3d5f5e0337b8cb77aaa053e5793
Geoff Crew <gc97@cornell.edu>
2015-05-29 22:32:26 +0000

    add ratings to song page

--- app/javascripts/songs/SongView.js
+++ app/javascripts/songs/SongView.js
@@ -3,7 +3,13 @@
 var BaseView = require('../base/BaseView');
 
 var SongView = BaseView.extend({
-    template: 'songs/song'
+    template: 'songs/song',
+
+    onDOM: function() {
+        var oldWidth = this.$('.rating-inner1').width(),
+            newWidth = oldWidth * this.model.get('rating') / 100;
+        this.$('.rating').width(newWidth);
+    }
 });
 
 module.exports = SongView;
