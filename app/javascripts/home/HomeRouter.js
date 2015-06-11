e0afb726722a1f46eda59b8f0ecd0ed9ab9e6c5c
Geoff Crew <gc97@cornell.edu>
2015-06-11 03:55:28 +0000

    show play collection on home view

--- app/javascripts/home/HomeRouter.js
+++ app/javascripts/home/HomeRouter.js
@@ -3,6 +3,7 @@
 var Metatags = require('brisket').Layout.Metatags;
 var RouterBrewery = require('../routing/RouterBrewery');
 var HomeView = require('./HomeView');
+var PlayCollection = require('../plays/PlayCollection');
 
 var HomeRouter = RouterBrewery.create({
 
@@ -14,15 +15,18 @@ var HomeRouter = RouterBrewery.create({
         var description = 'The home of Claquesous Radio';
         var title = 'Welcome to Claquesous Radio!';
 
-        return new HomeView()
-            .withTitle(title)
-            .withMetatags(new Metatags({
-                'description': description,
-                'og:title': title,
-                'og:description': description,
-                'twitter:title': title,
-                'twitter:description': description
-            }));
+        var plays = new PlayCollection();
+        return plays.fetch().then(function(){
+            return new HomeView({collection: plays})
+                .withTitle(title)
+                .withMetatags(new Metatags({
+                    'description': description,
+                    'og:title': title,
+                    'og:description': description,
+                    'twitter:title': title,
+                    'twitter:description': description
+                }));
+            });
     }
 
 });
--- app/javascripts/home/HomeView.js
+++ app/javascripts/home/HomeView.js
@@ -1,9 +1,17 @@
 'use strict';
 
 var BaseView = require('../base/BaseView');
+var PlaysCollectionView = require('../plays/PlayCollectionView');
 
 var HomeView = BaseView.extend({
-    template: 'home/home'
+    template: 'home/home',
+
+    beforeRender: function() {
+        this.createChildView(PlaysCollectionView)
+            .withOptions({collection: this.collection})
+            .andAppendIt();
+    }
+
 });
 
 module.exports = HomeView;
