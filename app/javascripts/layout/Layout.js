b02ecb8220537bc8564d53e94ce37cc4456be5f2
Geoff Crew <gc97@cornell.edu>
2015-06-17 03:46:42 +0000

    show tweet in layout

--- app/javascripts/layout/Layout.js
+++ app/javascripts/layout/Layout.js
@@ -4,6 +4,8 @@ var Brisket = require('brisket');
 var BaseView = require('../base/BaseView');
 var templates = require("../templating/templates").templates;
 var HoganAdapter = require("hogan-brisket")(templates);
+var PlayCollection = require('../plays/PlayCollection');
+var PlayView = require('../plays/PlayView');
 
 var Layout = Brisket.Layout.extend({
     templateAdapter: HoganAdapter,
@@ -14,6 +16,16 @@ var Layout = Brisket.Layout.extend({
 
     template: 'layout/layout',
 
+    fetchData: function(){
+        this.plays = new PlayCollection();
+        return this.plays.fetch({data: {limit: 1}});
+    },
+
+    beforeRender: function(){
+        this.createChildView(PlayView)
+            .withOptions({model: this.plays.at(0)})
+            .andInsertInto('.now-playing');
+    }
 });
 
 module.exports = Layout;
