f97902b683a7799d01f22eeb1d64afd96acbc30f
Geoff Crew <gc97@cornell.edu>
2015-07-04 00:47:23 +0000

    add songs to album view

--- app/javascripts/albums/AlbumView.js
+++ app/javascripts/albums/AlbumView.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var BaseView = require('../base/BaseView');
+var setRatings = require('../utils/ratings');
 var setTimes = require('../utils/timeago');
 
 var AlbumView = BaseView.extend({
     template: 'albums/album',
 
     onDOM: function(){
+        setRatings();
         setTimes();
     }
 
