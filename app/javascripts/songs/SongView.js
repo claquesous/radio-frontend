0de52978353e9b5052655dc60e94b246d86ea10b
Geoff Crew <gc97@cornell.edu>
2015-07-04 23:53:12 +0000

    use util for setting song ratings

--- app/javascripts/songs/SongView.js
+++ app/javascripts/songs/SongView.js
@@ -1,15 +1,14 @@
 'use strict';
 
 var BaseView = require('../base/BaseView');
+var setRatings = require('../utils/ratings');
 var setTimes = require('../utils/timeago');
 
 var SongView = BaseView.extend({
     template: 'songs/song',
 
     onDOM: function() {
-        var oldWidth = this.$('.rating-inner1').width(),
-            newWidth = oldWidth * this.model.get('rating') / 100;
-        this.$('.rating').width(newWidth);
+        setRatings();
         setTimes();
     }
 });
